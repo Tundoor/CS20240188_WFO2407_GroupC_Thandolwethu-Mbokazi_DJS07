abe047cba5da8d11ab437dd18516bce92c02135e
Thandolwethu <thandwem045@gmail.com>
2024-11-16 22:52:27 +0200

    Created Function that renders a random meme image on UI

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,9 +1,30 @@
-function Meme() {
+import React from "react"
+import memesData from "../../memesData"
 
+ function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    
+    
+    function getMemeImage() {
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
+    }
+    
     return (
       <>
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text"
                     placeholder="Shut up"
@@ -16,10 +37,13 @@ function Meme() {
                 />
                 <button 
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
-            </form>
+            </div>
+
+            <img src={meme.randomImage} className="meme--image" />
         </main>
       </>
     )
