bf9767e7c541d252c5f941a4b9abfc3f4469dc8a
Thandolwethu <thandwem045@gmail.com>
2024-11-16 20:01:29 +0200

    Cloned Repo and created header section

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+       <header className="header">
+            <img src="./images/troll-face.png" className="header--image"/>
+            <h2>Header component</h2>
+            <h4>React Course - Project 3</h4>
+        </header>
+    </>
+  )
+}
+
+export default App
