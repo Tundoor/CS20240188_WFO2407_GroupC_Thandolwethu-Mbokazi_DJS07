15f6ccedfef7296eec578801bbc6f4abd78c4394
Thandolwethu <thandwem045@gmail.com>
2024-11-16 20:22:14 +0200

    Created Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
+import Header from "./components/Header"
 
 function App() {
 
   return (
     <>
-       <header className="header">
-            <img src="./images/troll-face.png" className="header--image"/>
-            <h2>Header component</h2>
-            <h4>React Course - Project 3</h4>
-        </header>
+       <Header />
     </>
   )
 }
